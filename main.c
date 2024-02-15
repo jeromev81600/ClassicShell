f22e3adcf6d3edad4b9d5c4cc0669cbb56a1a1a7
jerome vilanova <jerome.vilanova@gmail.com>
2024-02-15 20:49:53 +0100

    Modification du code pour que le shell fonctionne sous windows

--- main.c
+++ main.c
@@ -1,25 +1,49 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <glob.h>
 
-#define TAILLE_BUFFER 150
-
-#ifdef _WIN32                                           //Préprocesseur défini par le compilateur lorsque le programme est sur windows
+//Préprocesseur défini par le compilateur lorsque le programme est sur windows
+#ifdef _WIN32
+    #include <windows.h>
     #define OS "Windows"
-#elif __linux__                                         //Préprocesseur défini par le compilateur lorsque le programme est sur Linux
+
+   void glob(const char *pattern, char *arg_list[], int *increment) {
+        WIN32_FIND_DATA findFileData;
+        HANDLE hFind = FindFirstFile(pattern, &findFileData);
+
+        if (hFind == INVALID_HANDLE_VALUE) {
+            printf("Aucun fichier correspondant au motif '%s' n'a été trouvé.\n", pattern);
+            return;
+        }
+
+        do {
+            if (!(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+                arg_list[*increment] = strdup(findFileData.cFileName);
+                (*increment)++;
+            }
+        } while (FindNextFile(hFind, &findFileData) != 0);
+
+        FindClose(hFind);
+    }
+
+//Préprocesseur défini par le compilateur lorsque le programme est sur Linux
+#elif __linux__
+    #include <unistd.h>
+    #include <sys/wait.h>
+    #include <errno.h>
+    #include <glob.h>
     #define OS "Linux"
+
+//Préprocesseur défini par le compilateur lorsque le programme ne correspond à aucun des deux
 #else
     #define OS "Unknown"
 #endif
 
+#define TAILLE_BUFFER 150
 
 int main()
 {
-      printf("Le programme s'exécute sur : %s\n", OS);
+      printf("Le programme s'execute sur : %s\n", OS);
       char    buffer[TAILLE_BUFFER];
 
     printf("\n Welcome to my command interpreter \n Tape \"exit\" to exit \n \n");
@@ -47,7 +71,11 @@ int main()
             }
             else                                        //Si le mot contient '*', la fonction glob() est utilisée pour rechercher les fichiers correspondant au motif. Les fichiers trouvés sont ensuite ajoutés à arg_list.
             {
-                glob_t     g;
+            #ifdef _WIN32
+                   glob(tmp, arg_list, &increment);
+             #else
+                // Traitement spécifique à Linux
+                glob_t g;
                 int retour_glob=glob(tmp,0,NULL,&g);
                 if (retour_glob==0)
                 {
@@ -59,10 +87,53 @@ int main()
                     }
                 }
                 globfree(&g);
+            #endif
             }
             tmp=strtok(NULL," ");
         }
         arg_list[increment]=NULL;
+
+        #ifdef _WIN32
+             // Créer un processus cmd.exe
+            PROCESS_INFORMATION piProcInfo;
+            STARTUPINFO siStartInfo;
+            BOOL bSuccess = FALSE;
+
+            // Initialiser les structures STARTUPINFO et PROCESS_INFORMATION.
+            ZeroMemory(&piProcInfo, sizeof(PROCESS_INFORMATION));
+            ZeroMemory(&siStartInfo, sizeof(STARTUPINFO));
+            siStartInfo.cb = sizeof(STARTUPINFO);
+
+            // Créer une commande avec les arguments.
+            char command[TAILLE_BUFFER] = "cmd.exe /C ";
+            strcat(command, buffer);
+
+            // Créer le processus
+            bSuccess = CreateProcess(NULL,
+                                     command,
+                                     NULL,
+                                     NULL,
+                                     FALSE,
+                                     0,
+                                     NULL,
+                                     NULL,
+                                     &siStartInfo,
+                                     &piProcInfo);
+
+            // Vérifier si le processus a été créé
+            if (!bSuccess) {
+                fprintf(stderr, "CreateProcess failed (%lu).\n", GetLastError());
+                return 1;
+            }
+
+            // Attendre que le processus se termine
+            WaitForSingleObject(piProcInfo.hProcess, INFINITE);
+
+            // Fermer les handles de processus et de thread.
+            CloseHandle(piProcInfo.hProcess);
+            CloseHandle(piProcInfo.hThread);
+        #else
+            // Traitement spécifique à Linux
         pid_t process=fork();                              // Création d'un nouveau processus (fork);
         if (process==0)
         {
@@ -74,6 +145,7 @@ int main()
         {
             wait(&process);
         }
+        #endif
         increment=0;
         while (arg_list[increment]!=NULL)
         {
